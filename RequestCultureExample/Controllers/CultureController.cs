e9fb5ae1ce47e387955b5d2cf36761d6d2c202b9
Michal Walczynski <mwalczynski@pgs-soft.com>
2019-07-14 17:26:28 +0200

    Add project files.

---
+++ RequestCultureExample/Controllers/CultureController.cs
@@ -0,0 +1,17 @@
+﻿namespace RequestCultureExample.Controllers
+{
+    using System;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CultureController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var date = DateTime.Now.ToString();
+            return Ok(date);
+        }
+    }
+}
---
+++ RequestCultureExample/Startup.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace RequestCultureExample
+{
+    using System.Globalization;
+    using Microsoft.AspNetCore.Localization;
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var supportedCultures = new[]
+            {
+                new CultureInfo("ar"),
+                new CultureInfo("en"),
+                new CultureInfo("en-US"),
+                new CultureInfo("en-GB"),
+                new CultureInfo("en-NZ"),
+                new CultureInfo("pl"),
+                new CultureInfo("pl-PL"),
+            };
+
+            services.Configure<RequestLocalizationOptions>(options =>
+            {
+                options.DefaultRequestCulture = new RequestCulture("en-US");
+                options.SupportedCultures = supportedCultures;
+                options.SupportedUICultures = supportedCultures;
+                options.RequestCultureProviders = new List<IRequestCultureProvider>
+                {
+                    new QueryStringRequestCultureProvider()
+                };
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseRequestLocalization();
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
